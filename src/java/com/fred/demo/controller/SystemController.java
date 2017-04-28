610f08da2c6fc13668655cc7e58e477d9205efcc
FredOracle <huxianmiao1985@sina.com>
2017-04-29 00:18:05 +0800

    complete project

---
+++ src/java/com/fred/demo/controller/SystemController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fred.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ */
+@Controller
+public class SystemController {
+
+    @RequestMapping("index")
+    public String index() {
+        System.out.println("======================");
+        return "index";
+    }
+
+    @RequestMapping("welcome")
+    public String welcome() {
+        System.out.println("======================");
+        return "welcome";
+    }
+}
